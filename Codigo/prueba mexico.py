e061b9984101afbc9300bf2ac077328874456ede
JaysonAMR <95949504+JaysonAMR@users.noreply.github.com>
2022-07-14 13:13:45 -0500

    Up New

--- Codigo/prueba mexico.py
+++ Codigo/prueba mexico.py
@@ -1,7 +1,8 @@
+import json
 import pandas as pd
 import requests
 import plotly.express as px
-df=pd.read_csv("D:\PSemestral_Topicos2\MexicoData.csv")
+df=pd.read_csv("C:\\Users\\jayso\\Documents\\GitHub\\Semestral_Topicos\\MexicoData.csv")
 repo_url = 'https://raw.githubusercontent.com/angelnmara/geojson/master/mexicoHigh.json' #Archivo GeoJSON
 mx_regions_geo = requests.get(repo_url).json()
 
@@ -10,10 +11,10 @@ fig = px.choropleth(data_frame=df,
                     locations='Estado', # nombre de la columna del Dataframe
                     featureidkey='properties.name',  # ruta al campo del archivo GeoJSON con el que se hará la relación (nombre de los estados)
                     color='Casos', #El color depende de las cantidades
-                    color_continuous_scale="burg", #greens
+                    color_continuous_scale="greens", #greens
                     #scope="north america"
                    )
-fig.update_geos(showcountries=True, showcoastlines=True, showland=True, fitbounds="locations")
+fig.update_geos(showcountries=False, showcoastlines=True, showland=False, fitbounds="locations")
 
 
 
